5a9d2e1938718332fcdc45b97a60d33c5c68e31d
Dawich Rodriguez <dawich.rodriguez@novatechdev.com>
2024-04-15 14:16:37 -0400

    Fix para tema de placas

--- Dgii.NcfApp/Services/SoapDgiiService.cs
+++ Dgii.NcfApp/Services/SoapDgiiService.cs
@@ -76,59 +76,47 @@ namespace Dgii.NcfApp.Services
         public async Task<GetPlacaResponse> GetPlacaResult(string rnc, string placa)
         {
             var soapXml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-                        <soap12:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap12=""http://www.w3.org/2003/05/soap-envelope"">
-                          <soap12:Body>
-                            <GetPlaca xmlns=""http://dgii.gov.do/"">
-                              <RNC>{rnc}</RNC>
-                              <Placa>{placa}</Placa>
-                              <IMEI></IMEI>
-                            </GetPlaca>
-                          </soap12:Body>
-                        </soap12:Envelope>";
+                             <soap12:Envelope xmlns:soap12=""http://www.w3.org/2003/05/soap-envelope"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+                                 <soap12:Body>
+                                     <GetPlaca xmlns=""http://dgii.gov.do/"">
+                                         <RNC>{rnc}</RNC>
+                                         <Placa>{placa}</Placa>
+                                         <IMEI></IMEI>
+                                     </GetPlaca>
+                                 </soap12:Body>
+                             </soap12:Envelope>";
 
-            var client = new RestClient("https://dgii.gov.do/wsMovilDGII/WSMovilDGII.asmx");
-
-            // Create RestRequest instance
-            var request = new RestRequest();
-            request.Method = Method.Post;
-            request.AddHeader("Content-Type", "application/soap+xml; charset=utf-8");
-            request.AddHeader("SOAPAction", "http://dgii.gov.do/GetPlaca");
-            request.AddParameter("application/soap+xml", soapXml, ParameterType.RequestBody);
-
-            // Execute the request
-            var response = await client.ExecuteAsync(request);
-
-            // Check response status
-            if (response.IsSuccessful)
+            using (var client = new HttpClient())
             {
-                try
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://dgii.gov.do/wsMovilDGII/WSMovilDGII.asmx");
+                request.Content = new StringContent(soapXml, System.Text.Encoding.UTF8, "application/soap+xml");
+
+                request.Headers.Add("SOAPAction", "http://dgii.gov.do/GetPlaca");
+
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    XmlSerializer serializer = new XmlSerializer(typeof(Envelope<GetPlacaResponse>), new XmlRootAttribute("Envelope"));
-                    using (StringReader reader = new StringReader(response.Content))
+                    try
                     {
-                        Envelope<GetPlacaResponse> envelope = (Envelope<GetPlacaResponse>)serializer.Deserialize(reader);
-
-                        // Access
-                        return envelope.Body;
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        var result = XmlClassHelper.MapXmlToGetPlacaResult(responseContent);
+                        return result;
                     }
-                        //await _dgiiHistoryService.CreatePlacaHistory(placaR);
-                 
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+
                 }
-                catch (Exception e)
+                else
                 {
-                    throw e;
+                    Console.WriteLine($"Request failed with status code: {response.StatusCode}");
+                    return null;
                 }
-
-            }
-            else
-            {
-
-                // Cambia esto para que devuelva algun error
-                Console.WriteLine($"Request failed with status code: {response.StatusCode}");
-                return null;
             }
         }
-
         public async Task<RncResult> GetRNC(string rnc)
         {
             var soapXml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
